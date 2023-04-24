0ce0529e9fb19b2c545a9eace9c342dcf3835f49
Michael T. Bacon <mbacon@renci.org>
2023-04-24 17:19:16 -0400

    Needs default args

--- dags/write_date.py
+++ dags/write_date.py
@@ -4,9 +4,12 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 
+from roger.dag_util import default_args
+
 with DAG(
         dag_id="write_date",
         schedule_interval="25 4 * * *",
+        default_args=default_args
 ) as dag:
 
     dummy = DummyOperator(task_id="dummy_task")
